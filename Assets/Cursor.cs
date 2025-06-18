30be5e24702207464d97f2bb2783c4375446085d
Blackbird12356 <moneyup1925@gmail.com>
2025-06-18 17:25:22 +0500

    Сделал меню
    Пока сделал меню но буду дорабатывать позже сегодня вечером, осталось ток сделать настройки, и ждем алиша с его картой

---
+++ Assets/Cursor.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
